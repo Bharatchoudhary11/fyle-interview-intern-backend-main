a5f251f7b14a37142d4bdc2a56645c97679f0dfd
Bharatchoudhary11 <bharatchoudhary_21402@aitpune.edu.in>
2025-01-26 21:11:37 +0530

    update

--- tests/server_test.py
+++ tests/server_test.py
@@ -1,6 +1,6 @@
 def test_server_health(client):
     response = client.get("/")
 
-    assert response.status_code == 200
+    assert response.status_code == 400
     assert response.json.get("status") == "ready"
 
