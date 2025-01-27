4b86f13c6e507c7cbce981a6d201e243121a7483
Bharatchoudhary11 <bharatchoudhary_21402@aitpune.edu.in>
2025-01-27 13:44:04 +0530

    update all principle api

--- core/models/principals.py
+++ core/models/principals.py
@@ -6,6 +6,7 @@ class Principal(db.Model):
     __tablename__ = 'principals'
     id = db.Column(db.Integer, db.Sequence('principals_id_seq'), primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
     created_at = db.Column(db.TIMESTAMP(timezone=True), default=helpers.get_utc_now, nullable=False)
     updated_at = db.Column(db.TIMESTAMP(timezone=True), default=helpers.get_utc_now, nullable=False, onupdate=helpers.get_utc_now)
 
